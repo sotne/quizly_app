84694ffe2fb7fd794bf0ee37179603fc7af9b47e
sotone <sotofabian227@gmail.com>
2023-01-31 13:27:54 -0500

    created a new user table

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,4 +1,14 @@
+const { User } = require('../../models')
+
 module.exports = (req, res) => {
     console.log(req.body)
-    res.send('post auth router register')
+    //first check to make sure the password and confirmpass are the same
+    if (req.body.password !== req.body.confirmPass){
+        res.send({error: 'your password do not match'})
+    }
+    // get the data from the request body
+    const {username, email, password} = req.body
+    const user = new User({ username, email, password})
+    user.save();
+    res.send(`New User Created - ${user.username}`);
 }
